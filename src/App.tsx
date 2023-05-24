29213ada7760b48eb3bfdb2a092adc71c4e207ff
Dimas Indra Jaya <dimaszxc101@gmail.com>
2023-05-24 07:28:16 +0700

    scond commit

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ function App() {
     setLoading(true);
 
     try {
-      // kita dapat mengambil data menggunakan libray axios (untuk menghungkan fe dengan be via api)
+      // kita dapat mengambil data menggunakan libray axios (untuk menghubungkan fe dengan be via api)
       // ref: https://axios-http.com/docs/intro
       const { status, data: fetchResult } = await axios.get<IBaseApiResult>(
         `${baseUrl}/${surahNumber}`
@@ -117,11 +117,13 @@ function App() {
   return (
     // PS: dalam react kita menggunakan ternary sebagai perandaian "?" if, ":" else
     // PS2: dalam suatu function react kita hanya dapat mengembalikan satu value sama seperti menggunakan fuction
-    <div className="bg-white w-[1400px] min-w-full">
+    <div className="bg-white w-[80vw] h-[auto] box-border p-8">
       <div>
         {/* sedang loading data dari api (mengambil data dari backend via GET method REST API) */}
         {loading ? (
-          "lagi loading ..."
+          <div className="text-gray-950 font-bold transition-all duration-1000">
+            "Sedang Memuat ..."
+          </div>
         ) : (
           <div>
             {/* loading sudah selesai data di temukan */}
@@ -133,26 +135,38 @@ function App() {
                     {quranResult.name.translation.id ||
                       "translation tidak di temukan"}
                   </span>
-                  <span>
+                  <span className="border-b-2 border-stone-500 pb-2">
                     {quranResult.name.transliteration.id ||
                       "translation tidak di temukan"}
                   </span>
                 </div>
-
                 <div>
                   {quranResult.verses.length > 0 ? (
                     quranResult.verses.map((verse, index) => (
-                      <div>
-                        <div>{`${index + 1}, ${verse.text.arab}`}</div>
-                        <div>{`${verse.text.transliteration.en}`}</div>
+                      <div className="grid grid-cols-1 w-[70vw] m-auto">
+                        {/* <div className="text-end p-8 ">{`${index + 1}, ${ */}
+                        <div className="p-8 grid-rows-1">
+                          <span className="text-start block">
+                            <div className="inline-block border border-stone-950 h-8 w-8 text-center rounded-full border-2 font-bold m-3">
+                              {index + 1}
+                            </div>
+                          </span>
+                          <div className="text-end block font-bold ">
+                            {verse.text.arab}
+                          </div>
+                        </div>
+                        <div className="text-start border-b-2 pt-10 font-serif font-thin italic">
+                          {`${verse.text.transliteration.en}`}
+                        </div>
                       </div>
                     ))
                   ) : (
                     <span>ayat tidak di temukan!</span>
                   )}
                 </div>
-                <div className="flex flex-row  justify-evenly text-white ">
+                <div className="flex flex-row  justify-around text-white mt-16">
                   <button
+                    className="bg-green-600 border-1 border-stone-800 w-32"
                     disabled={surahNumber === 1}
                     onClick={() => {
                       // DOM, ketika di klik panggil fungsi
@@ -162,6 +176,7 @@ function App() {
                     Mundur
                   </button>
                   <button
+                    className="bg-green-600 border-1 border-stone-800 w-32"
                     disabled={surahNumber === 114}
                     onClick={() => {
                       handleAddSurahNumber("next");
